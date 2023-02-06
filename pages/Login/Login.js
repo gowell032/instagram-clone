899af78175cf08f5fd8615f36dab29b22b83b586
gowell032 <gowell032@gmail.com>
2023-02-06 23:40:16 +0900

    Login Js 2/1

---
+++ pages/Login/Login.js
@@ -0,0 +1,5 @@
+const inputId = document.getElementById('id')
+const inputPw = document.getElementById('pw')
+const loginBtn = document.getElementById('loginBtn')
+
+const idRegExp = /^[A-Za-z0-9_][A-Za-z0-9._]*[@]{1}[a-z]*[.]+[a-z]{1,3}$/;
