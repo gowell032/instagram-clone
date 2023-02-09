e1b076992f9bf15589c491e6b32daf09b3ec9301
gowell032 <gowell032@gmail.com>
2023-02-09 22:49:48 +0900

    Login js 2/1

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -1,5 +1,8 @@
-const inputId = document.getElementById('id')
-const inputPw = document.getElementById('pw')
-const loginBtn = document.getElementById('loginBtn')
+const inputId = document.getElementById('userId'); //@가 포함 되어 있는지
+const inputPw = document.getElementById('userPw'); // 패스워드가 6글자 이상인지
+const acctiveLoginBtn = document.getElementById('loginBtn'); // 위에 값을 충족시 버튼 색 변경
 
-const idRegExp = /^[A-Za-z0-9_][A-Za-z0-9._]*[@]{1}[a-z]*[.]+[a-z]{1,3}$/;
+function checkIdPw() {
+    inputId.value.includes('@') && inputPw.value.length >= 6 ? 
+    window.location.href='http://127.0.0.1:5500/pages/Main/Main.html' : alert('아이디와 비밀번호를 확인해주세요');
+    }
